2618bb9d106b82293c7568bc4cd9ec01a2e63c4b
MicheleGravagnone <michelegravagnone@gmail.com>
2026-05-10 13:21:17 +0200

    Updated Task setter and getter methods

--- src/main/java/dev/michele/Task.java
+++ src/main/java/dev/michele/Task.java
@@ -17,24 +17,49 @@ public class Task {
         this.updatedAt = Instant.now();
     }
 
-    int getId() {
+    public Task(int id, String description, TaskStatus status, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
         return this.id;
     }
 
-    String getDescription() {
+    public String getDescription() {
         return this.description;
     }
 
-    void setDescription(String description) {
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    String getStatus() {
+    public String getStatus() {
         return this.status.toString();
     }
 
-    void setStatus(String str) {
+    public void setStatus(TaskStatus t) {
+        this.status = t;
+    }
+
+    public void setStatus(String str) {
         this.status = TaskStatus.from(str);
+    }
+
+    public Instant getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %-12s %s (created: %s)", id, "[" + status + "]", description, createdAt);
     }
     
 }
