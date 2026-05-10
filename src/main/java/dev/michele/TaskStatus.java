71ff0b42733bb27033081adf1ce4375ef3a33b1a
MicheleGravagnone <michelegravagnone@gmail.com>
2026-05-10 21:05:35 +0200

    Refactoring TaskStatus

--- src/main/java/dev/michele/TaskStatus.java
+++ src/main/java/dev/michele/TaskStatus.java
@@ -5,19 +5,19 @@ public enum TaskStatus {
 
     public static TaskStatus from(String str) {
         return switch(str.toLowerCase()) {
-            case "todo" -> TODO;
+            case "todo"        -> TODO;
             case "in-progress" -> IN_PROGRESS;
-            case "done" -> DONE;
-            default -> throw new IllegalArgumentException("Undefined status" + str);
+            case "done"        -> DONE;
+            default            -> throw new IllegalArgumentException("Undefined status" + str);
         };
     }
 
     @Override
     public String toString() {
         return switch(this) {
-            case TODO -> "todo";
+            case TODO        -> "todo";
             case IN_PROGRESS -> "in-progress";
-            case DONE -> "done";
+            case DONE        -> "done";
         };
     }
     
