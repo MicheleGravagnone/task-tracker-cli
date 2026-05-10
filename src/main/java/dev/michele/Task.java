38126a33cc11922de432dd61d6328c9d6e15f4d0
MicheleGravagnone <michelegravagnone@gmail.com>
2026-05-10 13:13:52 +0200

    Added Status enum and defined Task class

--- src/main/java/dev/michele/Task.java
+++ src/main/java/dev/michele/Task.java
@@ -1,5 +1,20 @@
 package dev.michele;
 
+import java.time.Instant;
+
 public class Task {
+    private int id;
+    private String description;
+    private TaskStatus status;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.status = TaskStatus.TODO;
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
     
 }
--- src/main/java/dev/michele/TaskStatus.java
+++ src/main/java/dev/michele/TaskStatus.java
@@ -1,5 +1,24 @@
 package dev.michele;
 
-public class TaskStatus {
+public enum TaskStatus {
+    TODO, IN_PROGRESS, DONE;
+
+    public static TaskStatus from(String str) {
+        return switch(str.toLowerCase()) {
+            case "todo" -> TODO;
+            case "in-progress" -> IN_PROGRESS;
+            case "done" -> DONE;
+            default -> throw new IllegalArgumentException("Undefined status" + str);
+        };
+    }
+
+    @Override
+    public String toString() {
+        return switch(this) {
+            case TODO -> "todo";
+            case IN_PROGRESS -> "in-progress";
+            case DONE -> "done";
+        };
+    }
     
 }
